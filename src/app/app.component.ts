cf448ae0c2aac2ac64b941167bff2f1badb2b15b
Mykhailo Datsko <mike@datsko.net>
2019-03-18 03:38:35 +0200

    setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { recul, ReculOn } from 'recul';
 
+@ReculOn('hello')
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo-recul-angular';
+  count = '0';
+
+  constructor() {
+    recul.on('a', a => {
+      console.log('on a 1', a);
+      this.count = a;
+    });
+
+    // this.$count.then(count => console.log('count', count));
+
+    console.log(this.count);
+  }
+
+  ngOnInit() {
+    recul.on('a', a => console.log('on a 2', a));
+  }
+
+  upCount() {
+    let a = recul.getValue('a') || 0;
+    recul.setValue('a', recul.getValue('a', 'a') + 1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page1Component, Page2Component } from './components';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    Page2Component
   ],
   imports: [
     BrowserModule,
